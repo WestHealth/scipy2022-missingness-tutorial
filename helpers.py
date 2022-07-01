1b5c4e46ae3d756ea917db1a10a3cb377991b6eb
Haw-minn Lu <hlu@westhealth.org>
2022-07-01 09:36:30 -0700

    Update to latest

--- helpers.py
+++ helpers.py
@@ -1,7 +1,18 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
+
 # A function to cause missingness in a given column optionally
 def clobber(df, column, probability, depends=[]):
+    """
+    This clobbers a given column in a dataframe.
+    Missingness can optionally be dependent on other column(s)
+    :param df: The dataframe to clobber
+    :param column: The column to clobber
+    :param probability: The probability if certain conditions are met to clobber
+    :param depends: Clobbering depends on listed columns. (Empty = MCAR)
+    :return: Returns a copy of the dataset after it is clobbered
+    """
     clob = df[column] == df[column]  # Always True
     for dep_column in depends:
         clob &= df[dep_column] > df[dep_column].median()
@@ -11,7 +22,15 @@ def clobber(df, column, probability, depends=[]):
     ret[column] = np.where(clob < rand, df[column], np.nan)
     return ret
 
+
 def stat_comparison(original, missing, column):
+    """
+    Compares the statistics two datasets of a certain column
+    :param original: Original dataset
+    :param missing: Dataset with missing or imputed values
+    :param column: Column to compare
+    :return: A nicely formatted panda's dataframe
+    """
     df = pd.DataFrame.from_dict(
         dict(
             mean=[original[column].mean(), missing[column].mean()],
@@ -25,32 +44,58 @@ def stat_comparison(original, missing, column):
     df["percentage"] = df["difference"] / df["Original"] * 100
     return df
 
+
 #
 # Helps highlight cells for hot deck imputation demonstration
 #
 
 def spotlight_donors(df, donors, missing=None):
+    """
+    Spotlights specific cells in a panda's data frame to indicate which celss are donors.
+    This is for the hot deck imputation demo
+    :param df: The dataframe
+    :param donors: Collection of rows that are indicies
+    :param missing: Highlight the missing value
+    :return: Returns the colorized dataframe
+    """
     s = df.style
+
     def green(x):
         return 'background-color: green; color: white'
+
     def darkgreen(x):
         return 'background-color: darkgreen; color: white'
+
     def lightgreen(x):
         return 'background-color: lightgreen'
+
     def yellow(x):
         return 'background-color: yellow'
+
     for each in donors.index:
-        s=s.applymap(green, subset=pd.IndexSlice[each, 'distance']).applymap(lightgreen, subset=pd.IndexSlice[each,'feature a'])
+        s = s.applymap(green, subset=pd.IndexSlice[each, 'distance']).applymap(lightgreen,
+                                                                               subset=pd.IndexSlice[each, 'feature a'])
         if 'regression' in df.columns:
-            s=s.applymap(darkgreen, subset=pd.IndexSlice[each, 'regression'])
+            s = s.applymap(darkgreen, subset=pd.IndexSlice[each, 'regression'])
             if missing:
-                s=s.applymap(yellow, subset=pd.IndexSlice[missing, 'regression'])
+                s = s.applymap(yellow, subset=pd.IndexSlice[missing, 'regression'])
     return s
 
+
 class ImputationDisplayer:
+    """
+    This class attaches to a Panda's dataframe a displayer class. When invoked,
+    it displays the first N lines of the dataframe with imputed values highlighted
+    """
+
     def __init__(self, df):
         self.missingness = df.applymap(lambda x: "background-color: paleturquoise" if pd.isnull(x) else "")
-        df.displayer = self # attach to dataframe
-    
+        df.displayer = self  # attach to dataframe
+
     def __call__(self, df, rows):
+        """
+        :param df: The dataframe to color
+        :param rows: Number of rows (N) to display
+        :return: first N rows of a dataframe with imputed values highlighted
+        """
         return df.iloc[0:rows].style.apply(lambda x: self.missingness.iloc[0:rows], axis=None)
